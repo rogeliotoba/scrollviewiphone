1afba21a81dcf52112da0c5795dc7775ef8b0975
Rogelio Torres <rogeliotoba@gmail.com>
2014-03-12 18:19:47 -0600

    Initial Commit

---
+++ scrollViewPageControl/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  scrollViewPageControl
+//
+//  Created by Rogelio Torres on 12/03/14.
+//  Copyright (c) 2014 Rogelio Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ scrollViewPageControlTests/scrollViewPageControlTests.m
@@ -0,0 +1,34 @@
+//
+//  scrollViewPageControlTests.m
+//  scrollViewPageControlTests
+//
+//  Created by Rogelio Torres on 12/03/14.
+//  Copyright (c) 2014 Rogelio Torres. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface scrollViewPageControlTests : XCTestCase
+
+@end
+
+@implementation scrollViewPageControlTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
